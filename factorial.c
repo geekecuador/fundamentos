a9e021e479d51dffa6208a71b31450d886a60747
David Pulloquinga <dapl@icloud.com>
2013-05-02 19:03:02 -0500

    factorial
    Factorial con el do-while

---
+++ factorial.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ncurses.h>
+#include <math.h>
+ int main(int argc, char const *argv[])
+ {
+   int x;
+ 	int contador;
+ 	int factorial;
+ 	contador=1;
+ 	factorial=1;
+ 	printf("Ingrese el numero para el factorial: \n");
+ 	scanf("%i",&x);
+ 	do
+ 		{
+ 			(factorial=factorial*contador++);
+
+ 		}
+ 	while
+ 		
+ 			(contador<=x);
+ 		
+ 	printf("El factorial es %i\n",factorial);
+ 	return 0;
+ }
