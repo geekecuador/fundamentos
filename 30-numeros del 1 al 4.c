e4b371d58d194f85cf1013e2ab0d2fd3ac2d6a2a
David Pulloquinga <dapl@icloud.com>
2013-05-04 11:43:12 -0500

    Create 30-numeros del 1 al 4.c

---
+++ 30-numeros del 1 al 4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+#define e 1
+#define f 2
+#define g 3
+#define h 4
+int main()
+{
+    int a=1,b=2,c=3,d=4;
+    printf("1234");
+    printf("%d%d%d%d",a,b,c,d);
+    printf("%d%d%d%d",e,f,g,h);
+return 0;
+}
