3059637e582ecad08a04d15a6b02b79fa90f31a0
David Pulloquinga <dapl@icloud.com>
2013-05-05 11:19:29 -0500

    Create 36-ascii.c

---
+++ 36-ascii.c
@@ -0,0 +1,15 @@
+/***************************/
+/*Nombre:David Pulloquinga*/
+/****Primero Software*****/
+/************************/
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+  {
+        int i;
+        char a[1];
+        for (i=48;i<=122;i++ )
+        printf("%c\t",i);
+		return 0;
+	}
